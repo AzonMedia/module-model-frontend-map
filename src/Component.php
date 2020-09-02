5ec898bcf5eee379422dd219ff7299c108d7a171
Vesko Kenashkov <vesko@azonmedia.com>
2020-09-03 00:02:17 +0300

    initial import

---
+++ src/Component.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Azonmedia\ModelFrontendMap;
+
+use Azonmedia\Components\BaseComponent;
+
+class Component extends BaseComponent
+{
+
+    protected const COMPONENT_NAME = "Model to frontend component mapping";
+    protected const COMPONENT_URL = 'https://model-frontend-map.packages.guzaba.org/';
+    protected const COMPONENT_NAMESPACE = __NAMESPACE__;
+    protected const COMPONENT_VERSION = '0.0.1';
+    protected const VENDOR_NAME = 'Azonmedia';
+    protected const VENDOR_URL = 'https://azonmedia.com';
+    protected const ERROR_REFERENCE_URL = 'https://github.com/AzonMedia/module-model-frontend-map/docs/ErrorReference/';
+}
